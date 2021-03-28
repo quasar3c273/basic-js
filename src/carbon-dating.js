00f885c6399324e73e860aa5917950f6d9570ffa
Mikhail <48333089+quasar3c273@users.noreply.github.com>
2021-03-28 23:36:52 +0300

    init: start task "Carbon dating"

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,11 @@
 const CustomError = require("../extensions/custom-error");
-
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = dateSample = (sampleActivity) => {
+  if (typeof sampleActivity !== "str") return false;
+  let Activity = parseFloat(sampleActivity);
+  if (Activity <= 0 || isNaN(Activity) || Activity > MODERN_ACTIVITY) return false;
+  k = 0.693 / HALF_LIFE_PERIOD;
+  t = Math.log2(MODERN_ACTIVITY / sampleActivity) / k;
 };
