bb049c4a4f669407a76a93a7560f2f3a79c33748
Mikhail <48333089+quasar3c273@users.noreply.github.com>
2021-03-28 23:52:49 +0300

    fix: use math.log

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,9 +2,10 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = dateSample = (sampleActivity) => {
-  if (typeof sampleActivity !== 'string' || !Number(sampleActivity) || sampleActivity > 15 || sampleActivity < 0) return false;
-  let Activity = parseFloat(sampleActivity);
-  k = 0.693 / HALF_LIFE_PERIOD;
-  t = Math.log2(MODERN_ACTIVITY / sampleActivity) / k;
+module.exports = function dateSample(sampleActivity) {
+  if (!Number(sampleActivity) || typeof sampleActivity !== 'string' ||sampleActivity > 15 || sampleActivity < 0) {
+    return false
+  }
+  let k = 0.693/HALF_LIFE_PERIOD
+  return Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivity)) / k)
 };
