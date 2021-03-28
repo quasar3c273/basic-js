66e35787f8529830d1c8f8e1ce2d6154442f46a9
Mikhail <48333089+quasar3c273@users.noreply.github.com>
2021-03-28 23:45:29 +0300

    feat: add valid on fires step

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,9 +3,8 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = dateSample = (sampleActivity) => {
-  if (typeof sampleActivity !== "str") return false;
+  if (typeof sampleActivity !== 'string' || !Number(sampleActivity) || sampleActivity > 15 || sampleActivity < 0) return false;
   let Activity = parseFloat(sampleActivity);
-  if (Activity <= 0 || isNaN(Activity) || Activity > MODERN_ACTIVITY) return false;
   k = 0.693 / HALF_LIFE_PERIOD;
   t = Math.log2(MODERN_ACTIVITY / sampleActivity) / k;
 };
