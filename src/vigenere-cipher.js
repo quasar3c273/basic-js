e8d87b145837906481b849c265caa065c40dcaf9
Mikhail <48333089+quasar3c273@users.noreply.github.com>
2021-03-29 22:50:03 +0300

    init: strat task "vigenere-cipher"

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,37 @@
 const CustomError = require("../extensions/custom-error");
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
+    constructor(status = true) {
+        this.status = status;
+    }
+
+    encrypt(message, key) {
+        const letters = alphabet.split('');
+        message = message.toUpperCase().split('');
+        key = key.toUpperCase().split('');
+        let j = 0;
+        for (let i = 0; i < message.length; i++) {
+            if (letters.indexOf(message[i]) != -1) {
+                message[i] = letters[(letters.indexOf(message[i]) + letters.indexOf(key[j])) % letters.length];
+                j = ++j % key.length;
+            }
+        }
+        return (this.status) ? message.join('') : message.reverse().join('');
+    }
+
+    decrypt(encryptedMessage, key) {
+        encryptedMessage = encryptedMessage.toUpperCase().split('');
+        key = key.toUpperCase().split('');
+        let j = 0;
+        for (let i = 0; i < encryptedMessage.length; i++) {
+            if (alphabet.indexOf(encryptedMessage[i]) != -1) {
+                encryptedMessage[i] = alphabet[(alphabet.indexOf(encryptedMessage[i]) + alphabet.length - alphabet.indexOf(key[j])) % alphabet.length];
+                j = ++j % key.length;
+            }
+        }
+        return (this.status) ? encryptedMessage.join('') : encryptedMessage.reverse().join('');
+    }
+};
 
 module.exports = VigenereCipheringMachine;
