6a31111dd911cb4df27056380e44f08a8ddf225f
Mikhail <48333089+quasar3c273@users.noreply.github.com>
2021-03-29 11:43:02 +0300

    fix: use  ES6  - `${}`

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -7,11 +7,11 @@ module.exports = function repeater(str, options) {
 
   for (let i = 0; i < options.additionRepeatTimes; i++) {
     const additionSeparator = options.additionSeparator ? options.additionSeparator : '|';
-    separatorStr += options.addition + i < options.additionRepeatTimes - 1 ? additionSeparator : '';
+    separatorStr += `${options.addition}${i < options.additionRepeatTimes - 1 ? additionSeparator : ''}`;
   }
   for (let i = 0; i < options.repeatTimes; i++) {
-    result += str + ' ' separatorStr + ' ' + i < options.repeatTimes - 1 ? separator : '';
+    result += `${str}${separatorStr}${i < options.repeatTimes - 1 ? separator : ''}`;
   }
 
-  return result ? result : str + options.addition;
+  return result ?  result : `${str}${options.addition}`;
 };
